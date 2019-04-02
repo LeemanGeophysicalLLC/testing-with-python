c9eaec0bb7eb569d8940d270f44406101d231121
J.R. Leeman <kd5wxb@gmail.com>
2019-04-02 09:53:56 -0600

    Add exercise sections

--- meteogram/tests/test_meteogram.py
+++ meteogram/tests/test_meteogram.py
@@ -5,105 +5,124 @@ import pytest
 from meteogram import meteogram
 
 
-def test_build_asos_request_url_single_digit_datetimes():
-    """
-    Test building URL with single digit month and day.
-    """
-    # Setup
-    # Exercise
-    # Verify
-    # Cleanup
-    pass
-
-
-def test_build_asos_request_url_double_digit_datetimes():
-    """
-    Test building URL with double digit month and day.
-    """
-    # Setup
-    # Exercise
-    # Verify
-    # Cleanup
-    pass
-
 #
-# Exercise 1 Stop Here
+# Instructor led introductory examples
 #
 
-def test_build_asos_request_url_defaults():
-    """
-    Test building URL with all defaults.
-    """
-    # Setup
-    # Exercise
-    # Verify
-    # Cleanup
-    pass
-
-
-def test_build_asos_request_url_default_start_only():
-    """
-    Test building URL with default start date.
-    """
-    # Setup
-    # Exercise
-    # Verify
-    # Cleanup
-    pass
-
-
-def test_build_asos_request_url_default_end_only():
-    """
-    Test building URL with default end date.
-    """
-    # Setup
-    # Exercise
-    # Verify
-    # Cleanup
-    pass
-
 #
-# Exercise 2 Stop Here
+# Exercise 1 - Fill out the following test cases
 #
 
-def test_download_asos_data():
-    """Test downloading ASOS data for station AMW with defaults."""
-    # Setup
-    # Exercise
-    # Verify
-    # Cleanup
-    pass
-
-
-def test_download_asos_data_in_future():
-    """Test for correct behavior when asking for non-existant (future) data."""
-    # Setup
-    # Exercise
-    # Verify
-    # Cleanup
-    pass
-
-
-def test_download_asos_data_start_after_end():
-    """Test for correct behavior when start and end times are reversed."""
-    # Setup
-    # Exercise
-    # Verify
-    # Cleanup
-    pass
+# def test_build_asos_request_url_single_digit_datetimes():
+#     """
+#     Test building URL with single digit month and day.
+#     """
+#     # Setup
+#     # Exercise
+#     # Verify
+#     # Cleanup
+#     pass
+#
+#
+# def test_build_asos_request_url_double_digit_datetimes():
+#     """
+#     Test building URL with double digit month and day.
+#     """
+#     # Setup
+#     # Exercise
+#     # Verify
+#     # Cleanup
+#     pass
 
 #
-# Exercise 3 Stop Here
+# Exercise 1 - Stop Here
 #
 
-def test_plotting_meteogram_defaults(load_example_asos):
-    """Test default meteogram plotting."""
-    # Setup
-    # Exercise
-    # Verify
-    # Cleanup
-    pass
+#
+# Exercise 2 - Add calculation tests here
+#
 
 #
-# Exercise 4 Stop Here
+# Exercise 2 - Stop Here
 #
+
+#
+# Exercise 3 - Fill out theses tests using our mock when necessary
+#
+
+# def test_build_asos_request_url_defaults():
+#     """
+#     Test building URL with all defaults.
+#     """
+#     # Setup
+#     # Exercise
+#     # Verify
+#     # Cleanup
+#     pass
+#
+#
+# def test_build_asos_request_url_default_start_only():
+#     """
+#     Test building URL with default start date.
+#     """
+#     # Setup
+#     # Exercise
+#     # Verify
+#     # Cleanup
+#     pass
+#
+#
+# def test_build_asos_request_url_default_end_only():
+#     """
+#     Test building URL with default end date.
+#     """
+#     # Setup
+#     # Exercise
+#     # Verify
+#     # Cleanup
+#     pass
+
+#
+# Exercise 3 - Stop Here
+#
+
+#
+# Exercise 4 - Add any tests that you can to increase the library coverage.
+# think of cases that may not change coverage, but should be tested for as well.
+#
+
+#
+# Exercise 4 - Stop Here
+#
+
+#
+# Instructor led example of image testing
+#
+
+#
+# Exercise 5 - Modify plotting routine and add tests
+#
+
+#
+# Exercise 6 - Refactor the URL builder tests that you can into a parameterized
+#              test and put that here. Remove the old tests.
+#
+
+#
+# Exercise 6 - Stop Here
+#
+
+#
+# Exercise 7 - Make a fixture for testing calculations
+#
+
+#
+# Exercise 7 - Stop Here
+#
+
+#
+# Exercise 8 - vcrpy, use it to record responses to data gathering tests.
+#              modify the code already written above.
+#
+
+# Demonstration of TDD here (time permitting)
